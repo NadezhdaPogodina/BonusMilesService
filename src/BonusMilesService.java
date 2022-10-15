52b20ee87702127e932903780c1823fc03d7cce2
Nadezhda <pogodinadia@inbox.ru>
2022-10-15 23:10:22 +0300

    Added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,21 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int price = cost;
+        int bonus = 20;
+
+        int miles = price / bonus;
+        return miles;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
